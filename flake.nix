69c384930de5e53daee567ba7ee37b3d5f0cb64c
Alexandria <28266167+alxpettit@users.noreply.github.com>
2023-06-20 01:31:49 -0600

    try

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    httm.url = "github:kimono-koans/httm";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs, httm }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = httm;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
+        };
+      }
+    );
+}
+
